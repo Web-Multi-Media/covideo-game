36f8194502574630128fbf203fd4824ec431fcae
ZzzooLoozzZ <zzzooloozzz@protonmail.com>
2020-04-14 12:17:41 +0200

    Update add room button

--- frontend/src/pages/SelectRoomScreen.jsx
+++ frontend/src/pages/SelectRoomScreen.jsx
@@ -1,11 +1,20 @@
 import React, {useEffect, useState} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import Button from "@material-ui/core/Button";
+import AddIcon from '@material-ui/icons/Add';
 import Rooms from "../component/Room/Rooms";
 import './SelectRoomScreen.css';
 import './Common.css';
 
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650
+  }
+});
+
 function SelectRoomScreen(props) {
   const [roomId, setRoomId] = useState('');
+  const classes = useStyles();
   const rooms = props.rooms;
 
   const createNewRoom = () => {
@@ -27,8 +36,14 @@ function SelectRoomScreen(props) {
   return (<React.Fragment>
     <Rooms rooms={rooms} joinRoom={joinRoom}/>
     <div className="inputLine">
-      <Button id="outlined-basic-name" className="margButt" variant="contained" color="primary" onClick={createNewRoom}>
-        New room
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        startIcon={<AddIcon />}
+        onClick={createNewRoom}
+      >
+        Add room
       </Button>
     </div>
   </React.Fragment>);
