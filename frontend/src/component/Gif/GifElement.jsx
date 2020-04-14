d2ad94b81061a60971c088ab5d4db13df44e523c
ZzzooLoozzZ <zzzooloozzz@protonmail.com>
2020-04-14 12:11:08 +0200

    Improve CSS

--- frontend/src/component/Gif/GifElement.jsx
+++ frontend/src/component/Gif/GifElement.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function GifElement(props) {
   return (<div className="gif-container">
-    <a >
+    <a>
       <img src={props.gif.images.fixed_width_downsampled.url} onClick={() => props.sendGif(props.gif.images.original.url)} className="gif"/>
     </a>
   </div>);
