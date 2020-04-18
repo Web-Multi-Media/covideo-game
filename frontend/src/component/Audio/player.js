5008037bef65d1489f2cb7f1374e9ec927cf0c7d
xavierfav <xavier.favory@gmail.com>
2020-04-18 15:07:23 +0200

    Refactor sound players creation

--- frontend/src/component/Audio/player.js
+++ frontend/src/component/Audio/player.js
@@ -1,39 +1,11 @@
-import {
-  userConnect, 
-  startRound, 
-  startRoundActivePlayer, 
-  guessWord, 
-  timerEndSoon, 
-  endRoundwellPlayed, 
-  endRoundlooser
-} from './sounds'
+import {soundPlayers} from './sounds'
 
 
 const playSound = (type) => {
-  switch(type) {
-    case 'userConnect':
-      userConnect.play(0.5);
-      break;
-    case 'startRound':
-      startRound.play(0.5);
-      break;
-    case 'startRoundActivePlayer':
-      startRoundActivePlayer.play(0.5);
-      break;    
-    case 'guessWord':
-      guessWord.play(0.5);
-      break;       
-    case 'timerEndSoon':
-      timerEndSoon.play(0.5);
-      break;
-    case 'endRoundwellPlayed':
-      endRoundwellPlayed.play(0.5);
-      break;
-    case 'endRoundlooser':
-      endRoundlooser.play(0.5);
-      break;
-  } 
+  let sound_player = soundPlayers[type];
+  if (sound_player !== undefined) {
+    sound_player.play(0.5);
+  }
 }
-
 
 export default playSound;
--- frontend/src/component/Audio/sounds.js
+++ frontend/src/component/Audio/sounds.js
@@ -13,24 +13,29 @@ const create_player = (url) => {
   )
 }
 
-export const userConnect = create_player(
-  "//freesound.org/data/previews/377/377017_1172853-lq.mp3"
-  );
-export const startRound = create_player(
-  "//freesound.org/data/previews/476/476175_6101353-lq.mp3"
-);
-export const startRoundActivePlayer = create_player(
-  "//freesound.org/data/previews/446/446142_758593-lq.mp3"
-);
-export const guessWord = create_player(
-  "//freesound.org/data/previews/341/341695_5858296-lq.mp3"
-);
-export const timerEndSoon = create_player(
-  "//freesound.org/data/previews/197/197713_3676780-lq.mp3"
-);
-export const endRoundwellPlayed = create_player(
-  "//freesound.org/data/previews/253/253172_4404552-lq.mp3"
-);
-export const endRoundlooser = create_player(
-  "//freesound.org/data/previews/404/404767_140737-lq.mp3"
-);
+export const soundPlayers = {
+  userConnect: create_player(
+    "//freesound.org/data/previews/377/377017_1172853-lq.mp3"
+  ),
+  userConnect: create_player(
+    "//freesound.org/data/previews/377/377017_1172853-lq.mp3"
+  ),
+  startRound: create_player(
+    "//freesound.org/data/previews/476/476175_6101353-lq.mp3"
+  ),
+  startRoundActivePlayer: create_player(
+    "//freesound.org/data/previews/446/446142_758593-lq.mp3"
+  ),
+  guessWord: create_player(
+    "//freesound.org/data/previews/341/341695_5858296-lq.mp3"
+  ),
+  timerEndSoon: create_player(
+    "//freesound.org/data/previews/197/197713_3676780-lq.mp3"
+  ),
+  endRoundwellPlayed: create_player(
+    "//freesound.org/data/previews/253/253172_4404552-lq.mp3"
+  ),
+  endRoundlooser: create_player(
+    "//freesound.org/data/previews/404/404767_140737-lq.mp3"
+  )
+}
