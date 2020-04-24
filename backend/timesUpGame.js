ff800f1854e857c9210dff0196020c9417c78ee3
worms19 <anselmidamien@gmail.com>
2020-04-24 17:59:26 +0200

    add snackBar for scoring

--- backend/timesUpGame.js
+++ backend/timesUpGame.js
@@ -431,7 +431,9 @@ function validateWord(ws, obj, room) {
         wordsValidated: room.wordsValidated.length,
         team1Score: room.scoreFirstTeam,
         team2Score: room.scoreSecondTeam,
-        gifUrl: room.gifUrl
+        gifUrl: room.gifUrl,
+        playerScoring: ws.playerName,
+        wordScoring: true
       }
     };
     let responseToSpecific = _.cloneDeep(responseToBroadCast);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import handleServerResponse from "./webSocket/rootedFunctions";
+import handleServerResponse, {updateState} from "./webSocket/rootedFunctions";
 import {useLocation} from 'react-router-dom';
 import {makeStyles} from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -49,6 +49,8 @@ function App() {
     },
     room: {
       name: '',
+      playerScoring: '',
+      wordScoring: false,
       id: '',
       gifUrl: '',
       roundDescription:[],
@@ -91,6 +93,10 @@ function App() {
     const obj = JSON.parse(response.data);
     console.log("Backend response: ", obj);
     handleServerResponse(obj, gameState, setGameState);
+  }
+
+  function updateState(message){
+    updateState(message, gameState, setGameState);
   }
 
   useEffect(() => {
@@ -249,6 +255,9 @@ function App() {
       {
         gameState.room.gameStarted && gameState.global.joinedRoom &&
           <GameScreen
+            updateState={updateState}
+            wordScoring={gameState.room.wordScoring}
+            playerScoring={gameState.room.playerScoring}
             currentPlayer={gameState.player}
             gameMaster={gameState.room.gameMaster}
             activePlayer={gameState.room.activePlayer}
